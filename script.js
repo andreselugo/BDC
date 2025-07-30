f03c115ce1990faa10438a56641f755d032113d1
andreselugo <lugoandreseduardo@gmail.com>
2025-07-29 23:08:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.card').forEach(c => c.classList.remove('abierta'))
-})
-function toggleCard(card){
-  card.classList.toggle('abierta')
+  document.querySelectorAll('.card').forEach(c => c.classList.remove('abierta'));
+});
+
+function toggleCard(card) {
+  card.classList.toggle('abierta');
 }
-function volverInicio(){
-  window.location.href='index.html'
+
+function volverInicio() {
+  window.location.href = 'index.html';
 }
-function mostrarPantalla(id){
-  document.getElementById('inicio')?.classList.add('seccion-oculta')
-  document.querySelectorAll('section')?.forEach(s=>s.classList.add('seccion-oculta'))
-  document.getElementById(id)?.classList.remove('seccion-oculta')
+
+function mostrarPantalla(id) {
+  document.getElementById('inicio')?.classList.add('seccion-oculta');
+  document.querySelectorAll('section')?.forEach(s => s.classList.add('seccion-oculta'));
+  document.getElementById(id)?.classList.remove('seccion-oculta');
 }
